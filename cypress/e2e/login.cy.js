17a8e81101ded67da0be2fcee9c0564806ab28a5
Anna Patterson <64214550+apatte@users.noreply.github.com>
2024-04-12 19:16:29 -0400

    Initial commit

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,21 @@
+let username = "stareast_user";
+let password = "Tester01";
+
+describe("User Login Flow", () => {
+  beforeEach(() => {
+    cy.intercept("POST", "https://api.demoblaze.com/login").as("loginAPI");
+  });
+  it("user logs in successfully with valid credentials", () => {
+    cy.visit("https://www.demoblaze.com/");
+    cy.get("#login2").click();
+    cy.get("#logInModal").should("be.visible");
+    cy.get("#loginusername").clear().type(username);
+    cy.get("#loginpassword").clear().type(password);
+    cy.get(".btn-primary").contains("Log in").click();
+    cy.wait("@loginAPI").then((object) => {
+      expect(object.response.statusCode).to.eq(200);
+    });
+    cy.get("#nameofuser").should("contain", username);
+    cy.get("#logout2").should("contain", "Log out");
+  });
+});
